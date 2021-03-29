f26e0b817cd6972d0d7b537c3976055fb1ccd541
msakuta <masahiro.sakuta@gmail.com>
2021-03-30 03:38:36 +0900

    Add links to load sample scripts

--- main.js
+++ main.js
@@ -15,4 +15,21 @@ document.getElementById("run").addEventListener("click", run);
 
 // Wait for the html to fully load before loading main.js, since it assumes static html elements exist.
 window.onload = () => {
+    const samples = document.getElementById("samples");
+
+    ["expr.dragon", "factorial.dragon", "fibonacci.dragon", "recurse.dragon", "mandel.dragon",
+     "str.dragon", "type.dragon",
+     "if.dragon", "for.dragon", "fn.dragon", "array.dragon", "array_reverse.dragon"]
+     .forEach(fileName => {
+        const link = document.createElement("a");
+        link.href = "#";
+        link.addEventListener("click", () => {
+            fetch("scripts/" + fileName)
+                .then(file => file.text())
+                .then(text => document.getElementById("input").value = text);
+        });
+        link.innerHTML = fileName;
+        samples.appendChild(link);
+        samples.append(" ");
+    })
 }
