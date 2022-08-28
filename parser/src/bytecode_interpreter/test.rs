ddc491e57184b60054df4d42c43515d8767e7049
msakuta <masahiro.sakuta@gmail.com>
2022-08-28 16:32:28 +0900

    Add a bunch of more test codes

--- parser/src/bytecode_interpreter/test.rs
+++ parser/src/bytecode_interpreter/test.rs
@@ -1,5 +1,5 @@
 use super::*;
-use crate::{compile, expr, Statement};
+use crate::{compile, expr, Statement, source};
 
 fn compile_expr(s: &str) -> Bytecode {
     let bytecode = compile(&[Statement::Expression(expr(s).unwrap().1)]).unwrap();
@@ -40,5 +40,21 @@ fn parens_eval_test() {
     assert_eq!(
         interpret(&compile_expr("  2*2 / ( 5 - 1) + 3")),
         Ok(Value::I64(4))
+    );
+}
+
+#[test]
+fn var_test() {
+    assert_eq!(
+        interpret(&compile(&source("var x = 42.; x +  2; ").unwrap().1).unwrap()),
+        Ok(Value::F64(44.))
+    );
+}
+
+#[test]
+fn var_assign_test() {
+    assert_eq!(
+        interpret(&compile(&source("var x = 42.; x=12").unwrap().1).unwrap()),
+        Ok(Value::I64(12))
     );
 }
