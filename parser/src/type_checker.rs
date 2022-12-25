f562468a9ea9df7a86fba9fc6448f82d2bce1f29
msakuta <masahiro.sakuta@gmail.com>
2022-12-25 21:54:21 +0900

    Fix a typo in binary_cmp_type
    Although I don't know if we really want to compare arrays
    with comparison operators..

--- parser/src/type_checker.rs
+++ parser/src/type_checker.rs
@@ -318,7 +318,7 @@ fn binary_cmp_type(lhs: &TypeDecl, rhs: &TypeDecl) -> Result<TypeDecl, ()> {
         (TypeDecl::Integer, TypeDecl::I64 | TypeDecl::I32)
         | (TypeDecl::I64 | TypeDecl::I32, TypeDecl::Integer) => TypeDecl::I32,
         (TypeDecl::Array(lhs), TypeDecl::Array(rhs)) => {
-            return binary_op_type(lhs, rhs);
+            return binary_cmp_type(lhs, rhs);
         }
         _ => return Err(()),
     };
